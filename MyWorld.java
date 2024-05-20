3b07a637641088a166c802f4e837230ae4d79fa8
alexyesilyurt <yesilyurtalexander@gmail.com>
2024-05-20 12:33:50 -0400

    initializing world

--- MyWorld.java
+++ MyWorld.java
@@ -19,13 +19,20 @@ public class MyWorld extends World
         super(600, 400, 1); 
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
     {
-        word word = new word();
-        addObject(word,169,186);
+        showText("Connections!", 300, 40);
+        showText("Singleplayer", 115, 295);
+        showText("Multiplayer", 470, 295);
+        
+        button1 button1 = new button1();
+        addObject(button1,115,249);
+        button2 button2 = new button2();
+        addObject(button2,470,240);
     }
 }
